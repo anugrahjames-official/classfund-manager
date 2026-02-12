fc5ff980ef6d90a7b6551743806d891467ffe83a
abdul basith p v <basithofficialmail01@gmail.com>
2026-02-12 22:48:42 +0530

    add background music and enhance styles for dashboard and login page

--- public/js/script.js
+++ public/js/script.js
@@ -28,5 +28,26 @@ loginBtn.addEventListener("click", () => {
       console.error(err.code, err.message);
     });
 });
+function startMusic(filePath) {
+  const bgm = new Audio(filePath);
+  bgm.loop = true;
+  bgm.volume = 0.15;
 
+  const playAudio = () => {
+    bgm.play()
+      .then(() => {
+        console.log("🔊 Retro BGM Active");
+        // Remove listeners so it only triggers once
+        window.removeEventListener('click', playAudio);
+        window.removeEventListener('touchstart', playAudio);
+      })
+      .catch(err => console.log("Waiting for user click..."));
+  };
+
+  window.addEventListener('click', playAudio);
+  window.addEventListener('touchstart', playAudio); 
+}
+
+// Call it
+startMusic('./bg.mp3');
 
