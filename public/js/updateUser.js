89092e2e539184ddc09d0bdef1373cc00e445ffb
abdul basith p v <basithofficialmail01@gmail.com>
2026-02-15 08:11:48 +0530

    add 404 error page and update user data functionality

---
+++ public/js/updateUser.js
@@ -0,0 +1,28 @@
+import { collection, query, where, getDocs,orderBy,increment,updateDoc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { db } from "./firebase.js";
+
+export async function getStudentData(inputRollNo) {
+  const usersRef = collection(db, "users");
+  const q = query(usersRef, where("rollNo", "==", inputRollNo));
+
+  try {
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      console.log("No student found with that Roll Number.");
+      return;
+    }
+
+
+    for (const docSnap of querySnapshot.docs) {
+      await updateDoc(docSnap.ref, {
+        totalPaid: increment(20)
+      });
+      console.log(`Updated Roll No: ${inputRollNo}`);
+    }
+
+  } catch (error) {
+    console.error("Error updating student:", error);
+  }
+}
+
